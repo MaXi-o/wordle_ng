3741b0f44b56d119157b8bcf9e6c706e02041038
Maximus Österman <maximus@oesterman.com>
2022-03-04 01:08:03 +0100

    Made settins page

--- wordle/src/app/app.module.ts
+++ wordle/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
-import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
