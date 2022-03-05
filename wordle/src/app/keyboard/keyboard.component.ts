1227d828b78ef9b2ab6940110d7f182664878c27
Emil <emil.logdberg@gmail.com>
2022-03-05 19:11:28 +0100

    contination of keypresses

--- wordle/src/app/keyboard/keyboard.component.ts
+++ wordle/src/app/keyboard/keyboard.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.html',
   styleUrls: ['./keyboard.component.css']
 })
-export class KeyboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class KeyboardComponent{
+  onKeypress(event: any){
+    console.log(event)
   }
 
 }
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let guess: string[] = ["a" ,"b"];
+console.log(guess.length)
+
+// function getResult(event:KeyboardEvent) {
+//   var keyEventPresses = event.key;
+//   console.log(keyEventPresses);
+
+
+//   if (alphabet.indexOf(keyEventPresses) != -1 && guess.length < 5) {
+//     guess.push(keyEventPresses);
+//     console.log(guess.length);
+//     console.log("guess is",guess)
+//   }
+
+//   if (keyEventPresses === "Enter" && guess.length === 5) {
+//     console.log("pressed enter");
+//   } else if (keyEventPresses === "Backspace") {
+//     console.log("delete last letter")
+//   }}
+
+//   getResult()
--- wordle/src/app/main/main.component.ts
+++ wordle/src/app/main/main.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class MainComponent{
+  guess!:string[]
+  @HostListener('window:keydown', ['$event']) spaceEvent(event: any){
+    console.log(event.key)
+    this.keyPressed = event.key;
+    console.log(event)
+    
+   if (event.keyCode >= 65 && event.keyCode <=90){
+    this.guess.push(this.keyPressed)
+   }
+   else {
+     console.log("fail")
+   }
+   console.log(this.guess)
   }
+  
+  keyPressed!: string;
 
 }
